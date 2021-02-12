55fabcc6f87998117f97bea910e50e09aeaf7f3a
Julian Patterson <julian.patterson@gmail.com>
2021-02-12 12:33:03 -0500

    commit tree e3449b3e38379d90e2556619968323d1e968c3ed

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :match_this
+    attr_accessor :string
+    def initialize(word)
+        @match_this = word
+    end
+
+    def match(array)
+        array.select {|word| word.split("").sort == @match_this.split("").sort}
+    end
+
+end
